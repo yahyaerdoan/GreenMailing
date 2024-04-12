649039424ed10d85f333922d4b1194d115474b3d
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-11 22:57:01 -0500

    Message entity created.

---
+++ GreenMailing.EntityLayer/Concrete/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GreenMailing.EntityLayer.Concrete
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Sender { get; set; }
+        public string Recipient { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+        public bool Status { get; set; }
+    }
+}
