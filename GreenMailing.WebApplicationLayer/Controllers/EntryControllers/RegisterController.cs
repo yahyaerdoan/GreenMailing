d25c9f4fe3b2769ff670a268f346d2627b50bf3d
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-17 16:04:25 -0500

    Layout reupdated.

---
+++ GreenMailing.WebApplicationLayer/Controllers/EntryControllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenMailing.WebApplicationLayer.Controllers.EntryControllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GreenMailing.WebApplicationLayer/Controllers/LayoutControllers/LayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenMailing.WebApplicationLayer.Controllers.LayoutControllers
+{
+    public class LayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
