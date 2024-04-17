d5e6c18d05e2bff664db5fbfd4ce82ea01bd331b
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-17 12:24:24 -0500

    Generic repository developed.

---
+++ GreenMailing.DataAccessLayer/Abstract/IGenericRepository/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenMailing.DataAccessLayer.Abstract.IGenericRepository
+{
+    public interface IGenericRepository<TEntity> where TEntity : class, new()
+    {
+        List<TEntity> GetAll(Expression<Func<TEntity, bool>>? filter = null);
+        TEntity Get(Expression<Func<TEntity, bool>> filter);
+        void Add(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
---
+++ GreenMailing.DataAccessLayer/Concrete/GenericRepository/EfGenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using GreenMailing.DataAccessLayer.Abstract.IGenericRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenMailing.DataAccessLayer.Concrete.GenericRepository
+{
+    public class EfGenericRepository<TEntity, TContext> : IGenericRepository<TEntity> where TEntity : class, new() where TContext : DbContext, new()
+    {
+        protected readonly DbContext context;
+        protected readonly DbSet<TEntity> set;
+
+        public EfGenericRepository(DbContext context)
+        {
+            this.context = context;
+            this.set = this.context.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            set.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            set.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            var values = set.FirstOrDefault(filter);
+            return values;
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>>? filter = null)
+        {
+            var values = set.Where(filter).ToList();
+            return values;
+        }
+
+        public void Update(TEntity entity)
+        {
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified; 
+            context.SaveChanges();
+        }
+    }
+}
