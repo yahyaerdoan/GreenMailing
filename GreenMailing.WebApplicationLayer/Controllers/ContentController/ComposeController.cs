210c9285606622789422cf7146d5465fa6ff598b
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-17 22:46:25 -0500

    Content template developed.

---
+++ GreenMailing.WebApplicationLayer/Controllers/ContentController/ComposeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenMailing.WebApplicationLayer.Controllers.ContentController
+{
+    public class ComposeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GreenMailing.WebApplicationLayer/Controllers/ContentController/ReadMailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenMailing.WebApplicationLayer.Controllers.ContentController
+{
+    public class ReadMailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
