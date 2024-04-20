7d4b972469fb767716e0f870835e7c1afdc4b429
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-20 14:12:08 -0500

    Worked on register.

--- GreenMailing.WebApplicationLayer/Controllers/EntryControllers/RegisterController.cs
+++ GreenMailing.WebApplicationLayer/Controllers/EntryControllers/RegisterController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GreenMailing.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GreenMailing.WebApplicationLayer.Controllers.EntryControllers
 {
     public class RegisterController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<User> _userManager;
+
+		public RegisterController(UserManager<User> userManager)
+		{
+			_userManager = userManager;
+		}
+
+        [HttpGet]
+		public IActionResult Index()
         {
             return View();
         }
-    }
+
+		[HttpPost]
+		public async  Task<IActionResult> CreateUser(User user)
+		{
+		
+			await _userManager.CreateAsync(user);
+			return RedirectToAction("Index", "LogIn");
+		}
+	}
 }
