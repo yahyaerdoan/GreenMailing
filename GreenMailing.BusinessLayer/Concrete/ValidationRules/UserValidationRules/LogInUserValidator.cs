cc18a1afec03231ec930764dcdcb7f78b24d4d5f
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-22 18:30:54 -0500

    Sign in method developed.

---
+++ GreenMailing.BusinessLayer/Concrete/ValidationRules/UserValidationRules/LogInUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using GreenMailing.DataTransferObjectLayer.Concrete.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenMailing.BusinessLayer.Concrete.ValidationRules.UserValidationRules
+{
+	public class LogInUserValidator : AbstractValidator<LogInUserDto>
+	{
+        public LogInUserValidator()
+        {
+			RuleFor(x => x.Email).NotEmpty().WithMessage("Email can not be empty!");
+			RuleFor(x => x.Password).NotEmpty().WithMessage("Password can not be empty!");
+		}
+    }
+}
--- GreenMailing.WebApplicationLayer/Controllers/EntryControllers/LogInController.cs
+++ GreenMailing.WebApplicationLayer/Controllers/EntryControllers/LogInController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GreenMailing.BusinessLayer.Concrete.ValidationRules.UserValidationRules;
+using GreenMailing.DataTransferObjectLayer.Concrete.Dtos;
+using GreenMailing.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GreenMailing.WebApplicationLayer.Controllers.EntryControllers
 {
     public class LogInController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<User> _userManager;
+		private readonly SignInManager<User> _signInManager;
+		LogInUserValidator _logInUserValidator = new();
+
+		public LogInController(UserManager<User> userManager, SignInManager<User> signInManager, LogInUserValidator logInUserValidator)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_logInUserValidator = logInUserValidator;
+		}
+		[HttpGet]
+		public IActionResult Index()
         {
             return View();
         }
-    }
+
+		[HttpPost]
+		public async Task<IActionResult> Index(LogInUserDto logInUserDtos)
+		{
+			var result = _logInUserValidator.Validate(logInUserDtos);
+			if (result.IsValid)
+			{
+				var userEmail = await _userManager.FindByEmailAsync(logInUserDtos.Email);
+				if (userEmail == null)
+				{
+					ModelState.AddModelError("", "Invalid email or password");
+					return View(logInUserDtos);
+				}
+				var userPassword = await _signInManager.PasswordSignInAsync(userEmail, logInUserDtos.Password, true, false);
+				if (userPassword.Succeeded) 
+				{ 
+					return RedirectToAction("Index", "Inbox");
+				}
+				else
+				{
+					ModelState.AddModelError("", "Invalid email or password");
+				}
+
+			}
+			return View(logInUserDtos);
+		}
+	}
 }
