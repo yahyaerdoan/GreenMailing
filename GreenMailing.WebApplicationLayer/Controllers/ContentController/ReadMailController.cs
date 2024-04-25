2a7783ba1cd82767a835f10dab7c558909de4f61
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-24 22:21:52 -0500

    Read mailbox developed. Route fixed.

--- GreenMailing.WebApplicationLayer/Controllers/ContentController/ReadMailController.cs
+++ GreenMailing.WebApplicationLayer/Controllers/ContentController/ReadMailController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GreenMailing.BusinessLayer.Abstract.IAbstractService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace GreenMailing.WebApplicationLayer.Controllers.ContentController
 {
     public class ReadMailController : Controller
     {
-        public IActionResult Index()
+        private readonly IMessageService _messageService;
+
+        public ReadMailController(IMessageService messageService)
         {
-            return View();
+            _messageService = messageService;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var values = _messageService.Get(x => x.MessageId == id);
+            return View(values);
         }
     }
 }
